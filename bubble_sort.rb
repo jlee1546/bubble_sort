5a02e92172776124ade267acc72e56de007259db
Jonathan Lee <jlee1546.jl@gmail.com>
2022-08-21 13:05:13 -0400

    Finish swapping logic

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,16 @@
-array = [3,2,1]
+array = [6,4,7,1,1,8]
 
-
-
-for index in 0..array.length - 1
-  if index == array.length - 1
-    break
-  elsif array[index] > array[index + 1]
-    temp = array[index]
-    array[index] = array[index + 1]
-    array[index + 1] = temp
-  
+#outside loop handles number of swaps inside traverses array
+for swap in 1..array.length - 1
+  for index in 0..array.length - 1
+    if index == array.length - 1
+      break
+    elsif array[index] > array[index + 1]
+      temp = array[index]
+      array[index] = array[index + 1]
+      array[index + 1] = temp
+    end
   end
-
 end
 
 p array
